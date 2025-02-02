fb3f4957a921290c259c1880e1b9c7bdd87ee944
Patrick Steppan <patrick.steppan@gmail.com>
2025-02-02 13:39:35 -0600

    rendering frame buffer, fixing free bug, getting fullscreen working

--- Main.c
+++ Main.c
@@ -11,16 +11,23 @@ int window_width = 800;
 int window_height = 600;
 
 uint32_t* frame_buffer;
+SDL_Texture* texture;
 
 bool initialize_window(void) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		fprintf(stderr, "Error initializing SDL.\n");
 		return false;
 	}
-	
+
+	// Query SDL for fullscreen max width + height
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
 	int x, y;
 	x = y = SDL_WINDOWPOS_CENTERED;
-	
 	// create SDL window
 	window = SDL_CreateWindow(NULL, x, y, window_width, window_height, SDL_WINDOW_BORDERLESS);
 
@@ -35,11 +42,24 @@ bool initialize_window(void) {
 		return false;
 	}
 
+	// SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
 	return true;
 }
 
 void setup(void) {
-	frame_buffer = (uint32_t)malloc(sizeof(uint32_t) * window_width * window_height);
+	frame_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+	if (!frame_buffer) {
+		fprintf(stderr, "Error allocating frame buffer.\n");
+	}
+
+	texture = SDL_CreateTexture(
+		renderer, 
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
 }
 
 void process_input(void) {
@@ -63,9 +83,31 @@ void update(void) {
 	// TODO
 }
 
+void clear_frame_buffer(uint32_t color) {
+	for (int i = 0; i < window_width * window_height; i++) {
+		frame_buffer[i] = color;
+	}
+}
+
+void render_frame_buffer(void) {
+	SDL_UpdateTexture(
+		texture,
+		NULL, // NULL for source rectangle
+		frame_buffer,
+		window_width * sizeof(uint32_t)
+	);
+
+	// NULL for source and destination rectangles
+	// because we want to render the entire texture
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+}
+
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 	SDL_RenderClear(renderer);
+
+	clear_frame_buffer(0xFFFFFF00);
+	render_frame_buffer();
 
 	SDL_RenderPresent(renderer);
 }
