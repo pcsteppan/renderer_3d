8d598ba9494350d56a384981854fc0f7f67340d2
pcsteppan <patrick.steppan@gmail.com>
2025-02-02 00:24:20 -0600

    adding quite and escape event handling

--- Main.c
+++ Main.c
@@ -37,6 +37,19 @@ void setup(void) {
 
 void process_input(void) {
 	// TODO
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				is_running = false;
+			}
+			break;
+	}
 }
 
 void update(void) {
