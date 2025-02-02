0b1678b977f49940e8da392e6b6ccef1df8a0a1b
pcsteppan <patrick.steppan@gmail.com>
2025-02-01 23:55:38 -0600

    Add project files.

---
+++ Main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL.h>
+
+bool is_running;
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error initializing SDL.\n");
+		return false;
+	}
+	
+	int x, y;
+	x = y = SDL_WINDOWPOS_CENTERED;
+	// create SDL window
+	window = SDL_CreateWindow(NULL, x, y, 800, 600, SDL_WINDOW_BORDERLESS);
+
+	if (!window) {
+		fprintf(stderr, "Error creating SDL window.\n");
+		return false;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char* args[]) {
+	is_running = initialize_window();
+	return 0;
+}
+
